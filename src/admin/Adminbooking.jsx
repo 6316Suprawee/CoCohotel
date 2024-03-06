d92fb496f7f4d3c88273a4ecaee88076292d8c63
phongram789 <gooddyps@gmail.com>
2024-03-07 00:52:38 +0700

    first step of Axios

--- src/admin/Adminbooking.jsx
+++ src/admin/Adminbooking.jsx
@@ -1,3 +1,25 @@
-export default function Adminbooking(){
-    return <h1>book</h1>
-}
+import React from 'react';
+import axios from 'axios';
+
+
+export default class Adminbooking extends React.Component {
+    state = {
+        persons: []
+    }
+    componentDidMount() { //เป็น lifecycle method ของ React component ที่ถูกเรียกหลังจาก component ถูก render ครั้งแรก
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+          .then(res => {
+            const persons = res.data;
+            this.setState({ persons });
+          })
+    }
+    render() {
+      return (
+        <>
+            {
+                this.state.persons.map(person =><li key={person.id}>{person.name}</li>)
+            }
+        </>
+      );
+    }
+  }
